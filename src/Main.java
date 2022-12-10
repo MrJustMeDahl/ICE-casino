a390542b37b22fc0e18e67561528c6761aead2ed
MrJustMeDahl <nico.dahl93@gmail.com>
2022-12-10 10:27:07 +0100

    Imported processing core in main

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
+import processing.core.*;
 public class Main {
+
+    public PApplet pApplet = new PApplet();
+
     public static void main(String[] args) {
         ProgramControl pc = new ProgramControl();
         pc.runCasino();
