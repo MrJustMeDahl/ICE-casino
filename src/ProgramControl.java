eeb46b29222665b20a8b64c65f32490bcbc07906
MrJustMeDahl <nico.dahl93@gmail.com>
2022-12-12 13:57:43 +0100

    Added DisplayOptions

--- src/ProgramControl.java
+++ src/ProgramControl.java
@@ -9,6 +9,7 @@ public class ProgramControl {
     private PApplet sketch;
     private PImage img;
     private PFont welcomeFont;
+    private PFont menuFont;
     MainMenu mainMenu;
     public static User currentUser;
     private ArrayList<String> allGames;
@@ -18,6 +19,7 @@ public class ProgramControl {
         this.allGames = new ArrayList<>(Arrays.asList("BlackJack", "Craps", "Roulette"));
         this.img = sketch.loadImage("Menu images/Loginbackground.jpg");
         this.welcomeFont = sketch.createFont("Georgia", 80);
+        this.menuFont = sketch.createFont("Georgia", 50);
         this.mainMenu = mainMenu;
     }
 
@@ -61,11 +63,40 @@ public class ProgramControl {
         sketch.image(this.img, 0,0, 1240, 780);
         sketch.textFont(this.welcomeFont);
         sketch.textAlign(sketch.CENTER);
+        sketch.fill(255);
         sketch.text("WELCOME TO", sketch.width/2, sketch.height/6);
         sketch.text("ICE CASINO", sketch.width/2, sketch.height/6 + 80);
     }
 
     private void displayOptions(){
-        
+        sketch.rectMode(sketch.CENTER);
+        if(sketch.mouseX > sketch.width/2 - 250 && sketch.mouseX < sketch.width/2 + 250 && sketch.mouseY > sketch.height/2 - 75 && sketch.mouseY < sketch.height/2 - 25) {
+            sketch.fill(0, 0, 220, 150);
+        } else {
+            sketch.fill(255, 150);
+            sketch.noStroke();
+        }
+        sketch.rect(sketch.width / 2, sketch.height / 2 - 50, 500, 75, 40);
+        if(sketch.mouseX > sketch.width/2 - 250 && sketch.mouseX < sketch.width/2 + 250 && sketch.mouseY > sketch.height/2 + 25 && sketch.mouseY < sketch.height/2 + 75) {
+            sketch.fill(0, 0, 220, 150);
+        } else {
+            sketch.fill(255, 150);
+            sketch.noStroke();
+        }
+        sketch.rect(sketch.width / 2, sketch.height / 2 + 50, 500, 75, 40);
+        sketch.stroke(0);
+        sketch.textFont(menuFont);
+        if(sketch.mouseX > sketch.width/2 - 250 && sketch.mouseX < sketch.width/2 + 250 && sketch.mouseY > sketch.height/2 - 75 && sketch.mouseY < sketch.height/2 - 25) {
+            sketch.fill(255);
+        } else {
+            sketch.fill(0);
+        }
+        sketch.text("Login", sketch.width/2, sketch.height/2 - 35);
+        if(sketch.mouseX > sketch.width/2 - 250 && sketch.mouseX < sketch.width/2 + 250 && sketch.mouseY > sketch.height/2 + 25 && sketch.mouseY < sketch.height/2 + 75) {
+            sketch.fill(255);
+        } else {
+            sketch.fill(0);
+        }
+        sketch.text("Create new account", sketch.width/2, sketch.height/2 + 62);
     }
 }
