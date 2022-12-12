06c0ea04bcc4bc8818bb3edf14ebca737c6195e7
CarstenJuhl <113057317+CarstenJuhl@users.noreply.github.com>
2022-12-12 14:51:12 +0100

    Changed Roulette class, removed textbox, changed UML

--- src/Roulette.java
+++ src/Roulette.java
@@ -8,10 +8,12 @@ import static java.lang.Thread.sleep;
 
 public class Roulette extends PApplet {
     PFont f;
-    ArrayList<Textbox> textboxes = new ArrayList<Textbox>();
     RouletteWheel readWheelData = new RouletteWheel();
     int rectX = height/2+1200;
     int rectY = width/2+130;
+
+    int cols = 9;
+    int rows = 4;
 
 //******
 // Roulette constructor
@@ -31,10 +33,13 @@ public class Roulette extends PApplet {
 //*******
         public void setup(){
             background(0,55,0);
+//******
+// Array of bettingbuttons
+//******
+
 
 
             image (loadImage("Pictures/RoulettePictures/Roulette.jpeg"), height/6,width/15);
-//            image (loadImage("RoulettePictures/BettingRoulette.jpeg"), height/2+400, width/2-900);
             f = createFont("Times New Roman",16);
 
 
@@ -52,9 +57,9 @@ public class Roulette extends PApplet {
 //******
             textFont(f, 16);
             fill(0,0,0,127);
-            rect(height / 2 + 310, width / 2 - 950, 200, 80);
+            rect(height / 2 + 310, width / 2 - 945, 200, 80);
             fill(255);
-            text("Press 'Enter' to finalize bet(s)\n And start the spin  ", height / 2 + 315, width / 2 - 920);
+            text("Press 'Enter' to finalize bet(s)\n And start the spin\n press 'q' to quit  ", height / 2 + 315, width / 2 - 920);
 
 //******
 // Setting a textbox in the lower left corner
@@ -67,8 +72,26 @@ public class Roulette extends PApplet {
 //******
 // Betting buttons
 //******
-        fill(150,150,150,191);
-        rect(rectX,rectY,75,75);
+
+            fill(150,150,150,191);
+
+            int [][] bettingButtons = new int[cols][rows];
+            int tempX = rectX;
+            for (int i = 0; i<9; i++) {
+                for (int j = 0; j < 4; j++) {
+
+                    rect(tempX, rectY, 75, 75);
+                    tempX += 80;
+
+
+                }
+
+                tempX = rectX;
+                rectY += 80;
+            }
+        rectX = height/2+1200;
+        rectY = width/2+130;
+
 
 
 
@@ -86,6 +109,8 @@ public class Roulette extends PApplet {
                 }
             }
         }
+
+
 //******
 // Place bet on table - run placeBet()
 //******
