6dd207d88ec50dad7575644089708f278c502fd1
DanyLoyal <cph-dk174@cphbusiness.dk>
2022-12-14 15:52:28 +0100

    456

--- src/Crabs.java
+++ src/Crabs.java
@@ -5,22 +5,59 @@ public class Crabs extends PApplet {
     public static void main(String[] args) {
         PApplet.main("Crabs");
     }
+    private int betAmount = 250;
+    private boolean betMade = false;
 
 public void settings(){
-    size(1800,900);
+    size(1600,900);
 
 }
     public void setup() {
         background(0);
             image (loadImage("Pictures/CrabsPictures/Craps_table_layout.jpeg"), 250, 1);
-            rect(50,200,100,100);
-            rect(50,50,100,100);
-
 
 
     }
-
+    public void chooseBet() {
+    rectMode(CENTER);
+    textAlign(CENTER);
+    
+        fill(255, 150);
+        rect(150, 350, 200, 80, 40);
+        rect(100, 420, 100, 40, 40);
+        rect(200, 420, 100, 40, 40);
+        textSize(25);
+        fill(0);
+        text("" + betAmount, 150, 360);
+        textSize(18);
+        text("Decrease", 100, 425);
+        text("Increase", 200, 425);
+        if(mousePressed && mouseX > 50 && mouseX < 150 && mouseY > 400 && mouseY < 440){
+            if(betAmount <= 0) {
+                betAmount = 0;
+            } else {
+                betAmount -= 250;
+            }
+        }
+        if(mousePressed && mouseX > 150 && mouseX < 250 && mouseY > 400 && mouseY < 440){
+            if(betAmount + 250 >= ProgramControl.currentUser.getBalance()){
+                betAmount = ProgramControl.currentUser.getBalance();
+            } else {
+                betAmount += 250;
+            }
+        }
+        fill(255, 150);
+        rect(150, 475, 200, 50, 40);
+        textSize(25);
+        fill(0);
+        text("Make bet", 150, 485);
+        if(mousePressed && mouseX > 50 && mouseX < 250 && mouseY > 450 && mouseY < 500){
+            betMade = true;
+        }
+    }
     public void draw() {
+
+    chooseBet();
 
 
 
